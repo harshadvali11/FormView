0825de6dc01e5d1d678d8ed3fea4bc540d7aa615
jspmern <jspmern@gmail.com>
2024-06-11 16:03:19 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class College(models.Model):
+    Cname=models.CharField(max_length=100)
+    Cprincipal=models.CharField(max_length=100)
+    Cloaction=models.CharField(max_length=100)
+    
